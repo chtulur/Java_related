0c9770c7fc4083f5898ce9b2f9d8d654d341581e
Greg Molnar <85894446+chtulur@users.noreply.github.com>
2023-02-26 16:55:23 +0100

    Add files via upload

---
+++ DataStructures/NonEmptyBST.java
@@ -0,0 +1,63 @@
+package DataStructures;
+
+public class NonEmptyBST<T extends Comparable<T>> implements Tree<T> {
+	T data;
+	Tree<T> left;
+	Tree<T> right;
+	
+	public NonEmptyBST(T element) {
+		data = element;
+		left = new EmptyBST<T>();
+		right = new EmptyBST<T>();
+	}
+	
+	public NonEmptyBST(T element, Tree<T> leftTree, Tree<T> rightTree) {
+		data = element;
+		left = leftTree;
+		right = rightTree;
+	}
+	
+	public boolean isEmpty() {
+		return false;
+	}
+	
+	public int cardinality() {
+		return 1 + left.cardinality() + right.cardinality(); //clever recursion for counting the size and not use a separate variable to keep track
+	}
+	
+	public boolean member(T element) {
+		if (data == element) {
+			return true;
+		} else {
+			if (element.compareTo(data) < 0) {
+				return left.member(element);
+			} else {
+				return right.member(element);
+			}
+		}
+	}
+	
+	public NonEmptyBST<T> add(T element) {
+		if (data == element) {
+			return this;
+		} else {
+			if (element.compareTo(data) < 0) {
+				return new NonEmptyBST<T>(data, left.add(element), right);
+			} else {
+				return new NonEmptyBST<T>(data,  left, right.add(element));
+			}
+			
+		}
+	}
+	
+	public T smallestElement() {
+		return left.isEmpty() ? data : left.smallestElement();
+	}
+	
+	public T biggestElement() {
+		return right.isEmpty() ? data : right.biggestElement(); 
+	}
+
+	
+	
+}
