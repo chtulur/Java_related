dcf3965c70f3bb203f6340700c533b786c95e111
Greg Molnar <85894446+chtulur@users.noreply.github.com>
2023-02-28 13:03:39 +0100

    Add files via upload

---
+++ TicTacToe/src/TicTacToe/Mark.java
@@ -0,0 +1,16 @@
+package TicTacToe;
+
+public enum Mark {
+	X('X'), O('O'), 
+	EMPTY('-'); 
+	
+	private final char symbol;
+	
+	Mark(char symbol) {
+		this.symbol = symbol;
+	}
+	
+	public char getSymbol() {
+		return symbol;
+	}
+}
